48c5319d73fe3459356ba8d9d77dc4fff312371c
Shubham-Sakar10 <81552299+Shubham-Sakar10@users.noreply.github.com>
2024-12-10 00:29:50 +0530

    Add files via upload

---
+++ cypress.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+
+module.exports = defineConfig({
+  chromeWebSecurity: false,
+  reporter: 'cypress-mochawesome-reporter',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
+      allureWriter(on, config);
+      return config;
+    },
+  },
+});
